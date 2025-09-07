764f105442927a1960cbeee26f2c915ee1ca5817
yogesh <yogeshjat8965@gmail.com>
2025-09-07 16:56:45 +0530

    Ready to deploy on Render

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const url = `https://realtime-code-editor-final.onrender.com`;
+const url = `https://livecoder-1-oe65.onrender.com/`;
 const interval = 30000;
 
 function reloadWebsite() {
